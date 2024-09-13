da295e6fab46fc1b276af89f019015434d075fad
Tyler Lutz <180100824+tyler-lutz@users.noreply.github.com>
2024-09-12 20:59:03 -0400

    feat(nvim): add nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.signcolumn = "yes"
